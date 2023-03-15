d888c8635b9c2f35680a7019fd27ac1abe870202
Alim <mohammad.alim@jalantechnologies.com>
2023-03-15 19:18:48 +0530

    Commit: Form  Updated

---
+++ main.js
@@ -0,0 +1,16 @@
+let myLibrary = [
+    {"name":"Hit Refresh","author":"Satya Nadella","pages": 272 }
+];
+
+function Book() {
+  constructor (book,author,pages){
+    this.book = book;
+    this.author = author;
+    this.pages = pages;
+
+  }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
